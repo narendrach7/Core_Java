e500d32d0e627eaefe340d37ccf48a41b36cdef2
narendrach7 <narendrach940@gmail.com>
2026-06-13 16:18:41 +0530

    java

---
+++ src/com/Accessmodifier/in/AccessModifier1.java
@@ -0,0 +1,10 @@
+package com.Accessmodifier.in;
+
+public class AccessModifier1 {
+	 public static String name="Narendra";
+	 public  static void Method1() {
+		System.out.println(name);
+		System.out.println("Method 1");
+	}
+
+}
